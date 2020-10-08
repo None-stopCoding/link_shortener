8c06b2f7733c1e86a62344bd0c6a5e504bfe29c1
Алексей Жуков <gkvalex7@gmail.com>
2020-10-08 11:03:31 +0300

    setting up dev/prod envs and configuring starting app process

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const config = require('config');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express()
 // добавляем новый middleware json дял корректного парсига тела запроса req.body
@@ -8,6 +9,14 @@ app.use(express.json({ extended: true }));
 app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/link', require('./routes/link.routes'));
 app.use('/t', require('./routes/redirect.routes'));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')));
+
+    app.get('*', (response, request) => {
+        response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = config.get('port') || 5000;
 
